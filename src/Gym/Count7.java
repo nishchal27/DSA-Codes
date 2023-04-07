113a13037856dbcab459bbd2e3a42869e0d75ba3
nishchal <singhnishchal27@gmail.com>
2023-04-07 21:18:51 +0530

    count 7

---
+++ src/Gym/Count7.java
@@ -0,0 +1,24 @@
+//https://course.acciojob.com/idle?question=de4d436a-d8e4-4b59-ba91-a2d138d040a4
+//Count 7
+package Gym;
+
+import java.util.*;
+
+public class Count7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(count7(n));
+        sc.close();
+    }
+
+    public static int count7(int n) {
+        if (n < 1)
+            return 0;
+        if (n % 10 == 7)
+            return 1 + count7(n / 10);
+        else
+            return count7(n / 10);
+
+    }
+}
